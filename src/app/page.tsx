b1198699b5495051cd04900c4b6fc4a26193c7c2
Shyam Bisht <bishtshyam135@gmail.com>
2024-04-16 19:08:40 +0100

    added next image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,22 @@ import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
 import "@uploadthing/react/styles.css";
 import { getImages } from "~/server/queries";
+import Image from "next/image";
 
 export default async function HomePage() {
   async function Images() {
     const images = await getImages();
     return (
-      <div className="m-4 flex flex-wrap gap-4">
+      <div className="m-4 h-60 flex justify-center flex-wrap gap-4">
         {images.map((image) => (
-          <div key={image.id} className="flex w-48 flex-col">
-            <img src={image.url} />
+          <div key={image.id} className="flex h-40 w-32 flex-col">
+            <Image
+              src={image.url}
+              style={{ objectFit: "contain" }}
+              alt={image.name}
+              height={160}
+              width={160}
+            />
             <div>{image.name}</div>
           </div>
         ))}
